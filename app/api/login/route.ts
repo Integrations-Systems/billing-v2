a4620cf3f52fd632e59aba39647dc92460fbd667
MiguelCastanedaDev <mickeycasta4@gmail.com>
2026-06-16 17:53:11 -0600

    test

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: NextRequest) {
 
     const response = await fetch(`http://78.14.5.157/v1/auth/login`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36' },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         "email": email,
         "password": password
@@ -18,15 +18,15 @@ export async function POST(request: NextRequest) {
 
      console.log("RES", response)
 
-    const json = await response.json()
+    //const json = await response.json()
 
-    console.log("JSON", json)
+    //console.log("JSON", json)
 
     if (!response.ok) {
       return NextResponse.json(
         {
           success: false,
-          message: json.error,
+          message: response,
         },
         {
           status: response.status,
@@ -37,21 +37,21 @@ export async function POST(request: NextRequest) {
     const res = NextResponse.json(
       {
         success: true,
-        message: json.message,
-        token: json.token,
-        expires_at: json.expires_at,
+        message: response,
+        token: response,
+        expires_at: response,
       },
       {
         status: response.status,
       }
     )
 
-    res.cookies.set("token", json.token, {
+    res.cookies.set("token", '', {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       path: "/",
-      expires: new Date(json.expires_at),
+      //expires: new Date(json.expires_at),
     });
 
     return res;
